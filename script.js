f55d0a88f419f67999bfc2d8107fa321053194d7
GitJohnFis <jfisiiahi69@gmail.com>
2025-03-10 20:26:43 +1300

    change all innerText values into location arrays

--- script.js
+++ script.js
@@ -66,9 +66,9 @@ function myWeapon() {
 }
 //creating an empty location function to take care of goTown and goStore repetions
 function update(location) {
-    button1.innerText = location["button text"];//use arrays instead of bracket notations "Go to store"
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
+    button1.innerText = location["button text"][0];//use arrays instead of bracket notations "Go to store"
+    button2.innerText = location["button text"][1];// change all innerText values into location arrays
+    button3.innerText = location["button text"][2];
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
