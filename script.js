7ca7a1140c8b4172c92ed025b7e6588bf836db55
GitJohnFis <jfisiiahi69@gmail.com>
2025-03-10 13:59:35 +1300

    Change basic weapon inventory, buttons, and stats in JS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//console.log("Hello World");
 console.log("Hello World");
+   let exp = 0;
+   let health = 100;
+   let armor = 10;
+    let gold = 50;
+   let currentWeaponIndex = 0;
+    //undeclared variable 
+      let fighting;
+      let BEaSthealth;
+       let inventory = ["stick"]
+    //Buttons interaction 
+    const button1 = document.getquerySelector("#button1");
+]   const button2 = document.getquerySelector("#button2");  
+    const button3 = document.getquerySelector("#button3");
