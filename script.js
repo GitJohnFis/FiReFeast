b41533b098ee70ca9eccfa43ede6937c87b17224
GitJohnFis <jfisiiahi69@gmail.com>
2025-03-11 11:28:19 +1300

    Introduce Math object and use random number generation in attack logic.

--- script.js
+++ script.js
@@ -206,7 +206,8 @@ function attack() { //Build attack function to update text message with monster 
 text.innerText = "The " + monsters[fighting].name + "attacks. ";
    text.innerText += " You attack it with your "  + weapon[currentWeaponIndex].name + ".";// On a new line in the `attack` function, used the addition assignment operator (+=) to add the string " You attack it with your <weapon>." to the text value.                                                      //Add a new text message for the weapon you are battling with ie atk 
     health -= monsters[fighting].level; //Update health to subtract monster's level.
-    monsterHealth -= weapons[currentWeaponIndex].power //
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * exp) + 1; //Introduce Math object and use random number generation in attack logic.
+
 }
 function dodge() {
 
