aae5a8fefb3f3d4ec4baf0f02a44304f112b3760
GitJohnFis <jfisiiahi69@gmail.com>
2025-03-27 20:21:42 +0000

    custom properties stylization in css

---
+++ config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                bgColor: "#0a0a23",
+                nuetralColor: "#ffffff",
+                healthColor: "#c70d0d",
+                buttonBgColor: "#feac32",
+                buttonBgImageColor: "linear-gradient(#fecc4c,#feac32)",
+            },
+            fontFamily: {
+                sans: ["Sans-serif, "],
+                body: []
+            }
+        }
+    }
+}
